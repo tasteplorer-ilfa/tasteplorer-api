33a1e6d14ea6b8b8bcbb2f816f42964be6b567d8
Ilham Alamsyah <ilhamalamsyah510@gmail.com>
2024-11-06 14:42:55 +0700

    feat(auth): add auth feature for handling login and register user

---
+++ src/modules/auth/auth.module.ts
@@ -0,0 +1,28 @@
+import { forwardRef, Module } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthResolver } from './auth.resolver';
+import { UserModule } from 'src/modules/user/user.module';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
+import { JwtStrategy } from './strategies/jwt.strategy';
+import { JwtAuthGuard } from './guards/jwt-auth.guard';
+
+@Module({
+  imports: [
+    forwardRef(() => UserModule),
+    PassportModule,
+    JwtModule.registerAsync({
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET_KEY'),
+        signOptions: {
+          expiresIn: `${configService.get('JWT_EXPIRES_IN')}`,
+        },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+  providers: [AuthResolver, AuthService, JwtStrategy, JwtAuthGuard],
+  exports: [AuthService],
+})
+export class AuthModule {}
