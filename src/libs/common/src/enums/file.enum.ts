19aef3d8da5880ceff049433a68e14d7fe6b303c
Ilham Alamsyah <ilhamalamsyah510@gmail.com>
2024-11-16 12:21:12 +0700

    feat(libs): add file enums

---
+++ src/libs/common/src/enums/file.enum.ts
@@ -0,0 +1,24 @@
+import { registerEnumType } from '@nestjs/graphql';
+
+export enum EnumFolder {
+  ARTICLE = 'articles',
+  BANNER = 'banners',
+  RECIPE = 'recipes',
+}
+
+export enum EnumService {
+  Cloudinary = 'Cloudinary',
+  S3Storage = 'S3storage',
+  Web3Storage = 'Web3storage',
+}
+
+// Register the enum type to be used in GraphQL schema
+registerEnumType(EnumFolder, {
+  name: 'EnumFolder',
+  description: 'Folders in Cloudinary where images can be uploaded',
+});
+
+registerEnumType(EnumService, {
+  name: 'EnumService',
+  description: 'Enum service which storage service that will be used',
+});
